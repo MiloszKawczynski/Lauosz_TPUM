8c3eb766f6bc452dbcad3bd62401e50b8540d9b9
Armez <zamylosz@gmail.com>
2025-04-04 21:10:34 +0200

    [Changed] Server - Added PurchaseRequest

---
+++ Files/SerwerDane/WebSocketMessage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerwerDane
+{
+    public static class WebSocketCommands
+    {
+        public const string Purchase = "PURCHASE";
+        public const string PurchaseResponse = "PURCHASE_RESPONSE";
+    }
+
+    public class PurchaseRequest
+    {
+        public int PlantId { get; set; }
+    }
+}
