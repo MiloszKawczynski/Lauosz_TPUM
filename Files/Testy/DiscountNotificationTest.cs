c103fc28381ca7496b2e6f75b4164fa4c66ffd9e
Armez <zamylosz@gmail.com>
2025-04-05 13:54:43 +0200

    Tests: Add communication tests

---
+++ Files/Testy/DiscountNotificationTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SerwerLogika;
+using System;
+
+namespace Testy
+{
+    [TestClass]
+    public class DiscountNotificationTest
+    {
+        [TestMethod]
+        public void ShouldNotifyAboutDiscount()
+        {
+            var notifier = new DiscountNotifier();
+            var mockObserver = new MockObserver();
+
+            using (notifier.Subscribe(mockObserver))
+            {
+                notifier.NotifyDiscount(0.2f);
+                Assert.AreEqual(0.2f, mockObserver.LastDiscount);
+            }
+        }
+
+        private class MockObserver : IObserver<float>
+        {
+            public float LastDiscount { get; private set; }
+
+            public void OnNext(float value) => LastDiscount = value;
+            public void OnError(Exception error) { }
+            public void OnCompleted() { }
+        }
+    }
+}
---
+++ Files/Testy/PurchaseIntegrationTest.cs
@@ -0,0 +1,26 @@
+﻿using Dane;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace Testy
+{
+    [TestClass]
+    public class PurchaseIntegrationTest
+    {
+        [TestMethod]
+        public async Task ShouldSuccessfullyPurchasePlant()
+        {
+            var service = new global::Dane.WebSocketDataService();
+            try
+            {
+                await service.ConnectAsync();
+                var response = await service.SendCommandAsync(1); // ID rośliny
+                Assert.AreEqual("PURCHASE_SUCCESS", response);
+            }
+            catch
+            {
+                Assert.Inconclusive("Test wymaga działającego serwera WebSocket");
+            }
+        }
+    }
+}
