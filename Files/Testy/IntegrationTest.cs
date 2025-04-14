2a8636c6261d1ffee40dd484a17a67ab2c084093
Nowogórska Laura <242479@edu.p.lodz.pl>
2025-04-14 12:34:10 +0200

    [Changed] One IntegrationTest class with methods

---
+++ Files/Testy/IntegrationTest.cs
@@ -0,0 +1,43 @@
+﻿using Dane;
+using SerwerPrezentacja;
+
+namespace Testy
+{
+    [TestClass]
+    public class IntegrationTest
+    {
+        WebSocketServer server = new();
+        AbstractWebSocketDataService service = AbstractWebSocketDataService.Create();
+
+        [TestMethod]
+        public async Task ShouldConnectToServer()
+        {
+            server.Start();
+            try
+            {
+                await service.ConnectAsync();
+                Assert.IsTrue(true);
+            }
+            catch
+            {
+                Assert.Inconclusive("Test wymaga działającego serwera WebSocket");
+            }
+            
+        }
+
+        [TestMethod]
+        public async Task ShouldSuccessfullyPurchasePlant()
+        {
+            try
+            {
+                await service.ConnectAsync();
+                var response = await service.SendCommandAsync(1); // ID rośliny
+                Assert.AreEqual("PURCHASE_SUCCESS", response);
+            }
+            catch
+            {
+                Assert.Inconclusive("Test wymaga działającego serwera WebSocket");
+            }
+        }
+    }
+}
